3d190dd6368d25272830da2a6bb337e544a892ca
Jul <lnacles@naver.com>
2023-03-10 11:02:33 +0900

    init backend/

---
+++ backend/src/main/java/com/danim/DanimApplication.java
@@ -0,0 +1,13 @@
+package com.danim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanimApplication.class, args);
+	}
+
+}
