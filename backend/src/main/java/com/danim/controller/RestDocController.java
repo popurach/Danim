fc1e5b9bcb2df20cbb6554f71f2a9532989bc04b
Jul <lnacles@naver.com>
2023-03-21 17:38:08 +0900

    [be] fix/rest_docs

--- backend/src/main/java/com/danim/controller/RestDocController.java
+++ backend/src/main/java/com/danim/controller/RestDocController.java
@@ -1,10 +1,19 @@
 package com.danim.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Controller
 @Slf4j
@@ -29,16 +38,52 @@ public class RestDocController {
 
     @GetMapping("/2")
     public ClassPathResource restDoc1() {
-        ClassPathResource classPathResource = new ClassPathResource(FILE_PATH);
+        ClassPathResource classPathResource = new ClassPathResource(FILE_PATH1);
         if (classPathResource.exists() == false) {
-            log.info("invaild filepath :{}", FILE_PATH);
+            log.info("invaild filepath :{}", FILE_PATH1);
             throw new IllegalArgumentException();
         }
         log.info("file path exists = {}", classPathResource.exists());
         return classPathResource;
     }
     @GetMapping("/3")
-    public String restDocd(){
+    public String restDocd3() throws IOException {
+        Resource[] resources = ResourcePatternUtils
+                .getResourcePatternResolver(new DefaultResourceLoader())
+                .getResources("classpath/**");
         return "**/index.html";
+    }
+    @GetMapping("/4")
+    public String restDocd4(){
+        
+        return htmlFileReader(FILE_PATH);
+    }
+    @GetMapping("/5")
+    public String restDocd5(){
+
+        return htmlFileReader(FILE_PATH1);
+    }
+
+    static String htmlFileReader(String pathStr){
+
+        ClassPathResource resource = new ClassPathResource(pathStr);
+        String resultcontent = "";
+        try {
+            InputStream inputStream = resource.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+
+            StringBuilder builder = new StringBuilder();
+            while (true) {
+                String line = br.readLine();
+                if(line == null ) break;
+                builder.append(line);
+            }
+
+            resultcontent = builder.toString();
+
+        } catch (IOException e) {
+
+        }
+        return resultcontent;
     }
 }
