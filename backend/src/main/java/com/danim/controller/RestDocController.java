9936931f7e556c3e07cc371554fcb8036c292d2a
Jul <lnacles@naver.com>
2023-03-21 16:16:25 +0900

    [BE] fix/rest_docs rest doc deploy

---
+++ backend/src/main/java/com/danim/controller/RestDocController.java
@@ -0,0 +1,13 @@
+package com.danim.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class RestDocController {
+    @GetMapping("/")
+    public String restDoc(){
+        return "/index.html";
+    }
+}
