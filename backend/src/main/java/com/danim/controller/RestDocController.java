5c9e56e3a81eb1f0faf50e48823a6e20329174d8
Jul <lnacles@naver.com>
2023-03-21 17:11:22 +0900

    [be] fix/rest_docs approch index.html

--- backend/src/main/java/com/danim/controller/RestDocController.java
+++ backend/src/main/java/com/danim/controller/RestDocController.java
@@ -1,18 +1,41 @@
 package com.danim.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@Slf4j
 public class RestDocController {
+    private final String FILE_PATH = "/BOOT-INF/classes/static/index.html";
+    private final String FILE_PATH1 = "/static/docs/index.html";
+
+
     @GetMapping("/1")
-    public String restDoc(){
-        return "/BOOT-INF/classes/static/index.html";
+    public ClassPathResource restDoc(){
+        ClassPathResource classPathResource = new ClassPathResource(FILE_PATH);
+        if(classPathResource.exists()==false){
+            log.info("invaild filepath :{}",FILE_PATH);
+            throw new IllegalArgumentException();
+        }
+        log.info("file path exists = {}", classPathResource.exists());
+        return classPathResource;
     }
+
+
+
+
     @GetMapping("/2")
-    public String restDob(){
-        return "/static/docs/index.html";
+    public ClassPathResource restDoc1() {
+        ClassPathResource classPathResource = new ClassPathResource(FILE_PATH);
+        if (classPathResource.exists() == false) {
+            log.info("invaild filepath :{}", FILE_PATH);
+            throw new IllegalArgumentException();
+        }
+        log.info("file path exists = {}", classPathResource.exists());
+        return classPathResource;
     }
     @GetMapping("/3")
     public String restDocd(){
