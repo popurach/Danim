dae1ba807e75731ba0a0f44c4f2b2e9acb75176c
DESKTOP-DOGVPUB\SSAFY <kares64@gmail.com>
2023-03-22 15:58:57 +0900

    [FE] feat: 프로필 사진 변경 추가

---
+++ frontend/lib/view_models/modify_profile_view_model.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ModifyProfileViewModel extends ChangeNotifier {
+  final controller = TextEditingController();
+  late final _imagePath;
+  var _selectedImageFile;
+  var _nickname;
+  var _canModify = false;
+
+  checkDuplicate() {
+    // TODO check Duplicate to Server
+    _canModify = true;
+    notifyListeners();
+  }
+
+  ModifyProfileViewModel() {
+    // TODO get nickname and ProfileImage from Server
+    _nickname = '호준진심펀치';
+    _imagePath = 'https://picsum.photos/200/300';
+    controller.text = _nickname;
+  }
+
+  sendModifyProfile() {
+    // TODO Modify Profile request to Server
+  }
+
+  changeProfileImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _selectedImageFile = File(pickedFile.path);
+      notifyListeners();
+    }
+  }
+
+  get isValid => RegExp('^[a-zA-Z가-힣0-9]{3,10}').hasMatch(nickname);
+
+  get imagePath => _imagePath;
+
+  get canModify => _canModify;
+
+  get selectedImageFile => _selectedImageFile;
+
+  set imagePath(value) {
+    _imagePath = value;
+    notifyListeners();
+  }
+
+  get nickname => _nickname;
+
+  set nickname(value) {
+    _nickname = value;
+    notifyListeners();
+  }
+}
