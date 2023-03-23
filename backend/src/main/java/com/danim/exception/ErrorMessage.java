6c13b96d8eabe3b802c4ccde5181622c1055f431
woojin <woojin1home@naver.com>
2023-03-23 10:20:23 +0900

    [BE] feat: Global Exception작업 수정

--- backend/src/main/java/com/danim/exception/ErrorMessage.java
+++ backend/src/main/java/com/danim/exception/ErrorMessage.java
@@ -7,20 +7,26 @@ import org.springframework.http.HttpStatus;
 //사용할 ErrorMessage를 정의하는곳, 개발자가 정의하여 추가해면 됨
 @Getter
 public enum ErrorMessage {
-    VALIDATION_FAIL_EXCEPTION( "입력 값의 조건이 잘못 되었습니다.", HttpStatus.BAD_REQUEST),
-    UNDEFINED_EXCEPTION( "정의되지 않은 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    BINDING_FAIL_EXCEPTION( "내부 서버에서 오류가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    VALIDATION_FAIL_EXCEPTION("입력 값의 조건이 잘못 되었습니다.", HttpStatus.BAD_REQUEST),
+    UNDEFINED_EXCEPTION("정의되지 않은 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    BINDING_FAIL_EXCEPTION("내부 서버에서 오류가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 
-    NOT_PERMISSION_EXCEPTION( "권한이 없거나 부족합니다.", HttpStatus.FORBIDDEN),
-    NOT_EXIST_ROUTE( "존재하지 않는 경로입니다.", HttpStatus.BAD_REQUEST),
+    NOT_PERMISSION_EXCEPTION("권한이 없거나 부족합니다.", HttpStatus.FORBIDDEN),
+    NOT_EXIST_ROUTE("존재하지 않는 경로입니다.", HttpStatus.BAD_REQUEST),
 
-    NOT_EXIST_USER("존재하지 않는 유저 입니다",HttpStatus.BAD_REQUEST);
+    NOT_EXIST_USER("존재하지 않는 유저 입니다", HttpStatus.BAD_REQUEST),
+
+    NOT_EXIST_TIMELINE_PAGING("존재하지 않는 타임라인 페이징의 페이지 입니다",HttpStatus.BAD_REQUEST),
+    NOT_EXIST_PHOTO("존재하지 않는 사진 입니다", HttpStatus.BAD_REQUEST),
+    
+    
+    NOT_EXIST_TIMELINE("존재하지 않는 타임라인 입니다", HttpStatus.BAD_REQUEST);
 
 
     private final String errMsg;
     private final HttpStatus httpStatus;
 
-    ErrorMessage( String errMsg, HttpStatus httpStatus) {
+    ErrorMessage(String errMsg, HttpStatus httpStatus) {
         //this.code = code;
         this.errMsg = errMsg;
         this.httpStatus = httpStatus;
--- backend/src/main/java/com/danim/service/TimeLineServiceImpl.java
+++ backend/src/main/java/com/danim/service/TimeLineServiceImpl.java
@@ -43,7 +43,7 @@ public class TimeLineServiceImpl implements TimeLineService {
 
     @Override
     //모든 최신 타임라인 얻어옴 , 페이징 x
-    public List<TimeLine> searchTimelineOrderBylatest() throws Exception {
+    public List<TimeLine> searchTimelineOrderBylatest() throws BaseException {
         //User now = userRepository.findById(uid).orElseThrow(() -> new Exception("존재하지 않는 유저"));
         //List<TimeLine> timeline = timeLineRepository.findAllByUserUidOrderByCreateTimeDesc(now).orElseThrow(() -> new Exception("모든 최신 타임라인 얻어오기 실패"));
         List<TimeLine> timeline = timeLineRepository.findAll(Sort.by(Sort.Direction.DESC, "createTime"));
@@ -52,25 +52,25 @@ public class TimeLineServiceImpl implements TimeLineService {
 
     @Override
     //나의 타임라인 얻어옴 , 페이징 x
-    public List<TimeLine> searchMyTimeline(Long uid) throws Exception {
+    public List<TimeLine> searchMyTimeline(Long uid) throws BaseException {
         User now = userRepository.findById(uid).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
-        List<TimeLine> timeline = timeLineRepository.findAllByUserUid(now).orElseThrow(() -> new Exception("타임라인 얻어오기 실패"));
+        List<TimeLine> timeline = timeLineRepository.findAllByUserUid(now).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_TIMELINE));
         return timeline;
     }
 
     //찾고자 하는 상대 유저의 public이지 않은 타임라인 탐색 하는 메서드
     @Override
-    public List<TimeLine> searchTimelineNotPublic(Long uid) throws Exception {
-        User now = userRepository.findById(uid).orElseThrow(() -> new Exception("존재하지 않는 유저"));
-        List<TimeLine> timeline = timeLineRepository.findAllByUserUidAndTimelinePublic(now, true).orElseThrow(() -> new Exception("타임라인 얻어오기 실패"));
+    public List<TimeLine> searchTimelineNotPublic(Long uid) throws BaseException {
+        User now = userRepository.findById(uid).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
+        List<TimeLine> timeline = timeLineRepository.findAllByUserUidAndTimelinePublic(now, true).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_TIMELINE));
         return timeline;
     }
 
     @Override
-    public TimelinePostOuter searchOneTimeline(Long uid) throws Exception {
+    public TimelinePostOuter searchOneTimeline(Long uid) throws BaseException {
 
         //해당 되는 타임라인을 얻어 왔고
-        TimeLine now = timeLineRepository.findById(uid).orElseThrow(() -> new Exception("존재하지 않는 타임라인 입니다."));
+        TimeLine now = timeLineRepository.findById(uid).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_TIMELINE));
 
         //이제 그 다음으로 해당 되는 타임라인을 포스트를 얻어 올거임
         List<Post> post = postRepository.findAllByTimelineIdOrderByCreateTimeAsc(now);
@@ -119,11 +119,11 @@ public class TimeLineServiceImpl implements TimeLineService {
     }
 
     @Override
-    public void makenewTimeline(Long uid) throws Exception {
+    public void makenewTimeline(Long uid) throws BaseException {
         //여기서 넘어온 uid는 User의 uid아이디 입니다.
         TimeLine timeline = new TimeLine();
         //이렇게 해서 User를 찾아옴
-        User now = userRepository.findById(uid).orElseThrow(() -> new Exception("존재하지 않는 유저"));
+        User now = userRepository.findById(uid).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
 
         //새로운 타임라인 생성이 가능한다
         timeline.setUserUid(now);
@@ -132,9 +132,9 @@ public class TimeLineServiceImpl implements TimeLineService {
 
 
     @Override
-    public void finishTimeline(Long uid) throws Exception {
+    public void finishTimeline(Long uid) throws BaseException {
 
-        TimeLine now = timeLineRepository.findById(uid).orElseThrow(() -> new Exception("존재하지 않는 타임라인 입니다."));
+        TimeLine now = timeLineRepository.findById(uid).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_TIMELINE));
         //타임라인 완료 변경 작업 진행
 
         now.setComplete(Boolean.TRUE);
@@ -144,15 +144,15 @@ public class TimeLineServiceImpl implements TimeLineService {
 
 
     @Override
-    public void deleteTimeline(Long uid) throws Exception {
+    public void deleteTimeline(Long uid) throws BaseException {
 
-        TimeLine now = timeLineRepository.findById(uid).orElseThrow(() -> new Exception("존재하지 않는 유저"));
+        TimeLine now = timeLineRepository.findById(uid).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
         timeLineRepository.delete(now);
     }
 
     @Override
     public void changePublic(Long uid) throws Exception {
-        TimeLine now = timeLineRepository.findById(uid).orElseThrow(() -> new Exception("존재하지 않는 타임라인 입니다"));
+        TimeLine now = timeLineRepository.findById(uid).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_TIMELINE));
         Boolean temp = now.getTimelinePublic();
 
         //완료->비완료 , 비완료->완료 로 변경하는 작업
@@ -168,11 +168,11 @@ public class TimeLineServiceImpl implements TimeLineService {
 
     //타임라인 중에서 완료가 된 여행과 공개가 된 여행을 페이징 처리르 하여 보여준다 => 메인 피드 화면에서 타임라인과 썸네일 같이 넘어감
     @Override
-    public List<MainTimelinePhotoDto> searchTimelineOrderBylatestPaging(Pageable pageable) throws Exception {
+    public List<MainTimelinePhotoDto> searchTimelineOrderBylatestPaging(Pageable pageable) throws BaseException {
 
         Page<TimeLine> timeline = timeLineRepository.findAllByCompleteAndTimelinePublic(true, true, pageable);
         if (timeline.getContent().size() == 0) {
-            throw new Exception("존재하지 않는 타임라인 페이징의 페이지 입니다");
+            throw new BaseException(ErrorMessage.NOT_EXIST_TIMELINE_PAGING);
         }
 
         //이제 얻어낸 타임라인 리스트에 해당 되는 포스트 정보를 불러오도록 한다.
@@ -186,8 +186,8 @@ public class TimeLineServiceImpl implements TimeLineService {
             if (startpost == null || lastpost == null)
                 continue;
             //현재는 우선 임시로 작업을 하여 넣어 줄것으로 생각을 하고 있다.
-            Photo photo = photoRepository.findById(startpost.getPhotoList().get(0).getPhotoId()).orElseThrow(() -> new Exception("존재하지 않는 사진 입니다"));
-            User user = userRepository.findById(1L).orElseThrow(() -> new Exception("존재 하지 않는 유저입니다"));
+            Photo photo = photoRepository.findById(startpost.getPhotoList().get(0).getPhotoId()).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_PHOTO));
+            User user = userRepository.findById(1L).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
             MainTimelinePhotoDto temp = MainTimelinePhotoDto.builder(time, startpost, lastpost, photo, user).build();
             list.add(temp);
         }
@@ -197,8 +197,8 @@ public class TimeLineServiceImpl implements TimeLineService {
 
     //나의 타임라인 검색시 페이징 처리해서 검색을 해온다 => 나의 타임라인 조회를 할시에 비어 있는 타임라인으로 넘겨줄거임
     @Override
-    public List<MainTimelinePhotoDto> searchMyTimelineWithPaging(Long uid, Pageable pageable) throws Exception {
-        User now = userRepository.findById(uid).orElseThrow(() -> new Exception("존재하지 않는 유저"));
+    public List<MainTimelinePhotoDto> searchMyTimelineWithPaging(Long uid, Pageable pageable) throws BaseException {
+        User now = userRepository.findById(uid).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
         Photo photo = null;
         User user = null;
         Post post = null;
@@ -206,7 +206,7 @@ public class TimeLineServiceImpl implements TimeLineService {
 
         Page<TimeLine> timeline = timeLineRepository.findAllByUserUidOrderByCreateTimeDesc(now, pageable);
         if (timeline.getContent().size() == 0) {
-            throw new Exception("존재하지 않는 타임라인 페이징의 페이지 입니다");
+            throw new BaseException(ErrorMessage.NOT_EXIST_TIMELINE_PAGING);
         }
         //이제 얻어낸 타임라인 리스트에 해당 되는 포스트 정보를 불러오도록 한다.
         List<MainTimelinePhotoDto> list = new ArrayList<>();//넘겨줄 timeline dto생성
@@ -226,13 +226,13 @@ public class TimeLineServiceImpl implements TimeLineService {
                     lastpost = new Post();
                     lastpost.setAddress2("");
                 }
-                user = userRepository.findById(1L).orElseThrow(() -> new Exception("존재 하지 않는 유저입니다"));
+                user = userRepository.findById(1L).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
                 temp = MainTimelinePhotoDto.builder(time, startpost, lastpost, photo, user).build();
                 list.add(temp);
             } else {
                 //현재는 우선 임시로 작업을 하여 넣어 줄것으로 생각을 하고 있다.
-                photo = photoRepository.findById(startpost.getPhotoList().get(0).getPhotoId()).orElseThrow(() -> new Exception("존재하지 않는 사진 입니다"));
-                user = userRepository.findById(1L).orElseThrow(() -> new Exception("존재 하지 않는 유저입니다"));
+                photo = photoRepository.findById(startpost.getPhotoList().get(0).getPhotoId()).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_PHOTO));
+                user = userRepository.findById(1L).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
                 temp = MainTimelinePhotoDto.builder(time, startpost, lastpost, photo, user).build();
                 list.add(temp);
             }
@@ -243,7 +243,7 @@ public class TimeLineServiceImpl implements TimeLineService {
     //상대 타임라인 조회시 with Paging
     @Override
     public List<MainTimelinePhotoDto> searchTimelineNotPublicWithPaging(Long uid, Pageable pageable) throws Exception {
-        User user = userRepository.findById(uid).orElseThrow(() -> new Exception("존재하지 않는 유저"));
+        User user = userRepository.findById(uid).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
         Page<TimeLine> timeline = timeLineRepository.findAllByUserUidAndTimelinePublic(user, true, pageable);
         if (timeline.getContent().size() == 0) {
             throw new Exception("존재하지 않는 타임라인 페이징의 페이지 입니다");
@@ -277,7 +277,7 @@ public class TimeLineServiceImpl implements TimeLineService {
                 list.add(temp);
             } else {
                 //현재는 우선 임시로 작업을 하여 넣어 줄것으로 생각을 하고 있다.
-                photo = photoRepository.findById(startpost.getPhotoList().get(0).getPhotoId()).orElseThrow(() -> new Exception("존재하지 않는 사진 입니다"));
+                photo = photoRepository.findById(startpost.getPhotoList().get(0).getPhotoId()).orElseThrow(() -> new BaseException(ErrorMessage.NOT_EXIST_USER));
                 temp = MainTimelinePhotoDto.builder(time, startpost, lastpost, photo, user).build();
                 list.add(temp);
             }
