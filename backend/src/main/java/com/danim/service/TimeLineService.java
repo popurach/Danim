a84da3326ec5915aa18bfa7148dfa77ab94cbd43
woojin <woojin1home@naver.com>
2023-03-27 10:32:36 +0900

    [BE] fix: User변경 후 Timeline 에서  관련한 작업 진행

--- backend/src/main/java/com/danim/service/TimeLineService.java
+++ backend/src/main/java/com/danim/service/TimeLineService.java
@@ -1,8 +1,9 @@
 package com.danim.service;
 
-import com.danim.dto.MainTimelinePhotoDto;
+import com.danim.dto.MainTimelinePhotoDtoRes;
 import com.danim.dto.TimelinePostOuter;
 import com.danim.entity.TimeLine;
+import com.danim.entity.User;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public interface TimeLineService {
     List<TimeLine> searchTimelineOrderBylatest() throws Exception;
 
     //내 피드에서 내 타임라인 리스트 조회 ,
-    List<TimeLine> searchMyTimeline(Long uid) throws Exception;
+    List<TimeLine> searchMyTimeline(Long uid, User user) throws Exception;
 
     //다른 유저의 피드에서 타임라인 조회도 가능
     List<TimeLine> searchTimelineNotPublic(Long uid) throws Exception;
@@ -22,7 +23,7 @@ public interface TimeLineService {
     TimelinePostOuter searchOneTimeline(Long uid) throws Exception;
 
     //여행시작 =>완료
-    void makenewTimeline(Long uid) throws Exception;
+    void makenewTimeline(User user) throws Exception;
 
     //여행끝 =>완료
     void finishTimeline(Long uid) throws Exception;
@@ -35,13 +36,13 @@ public interface TimeLineService {
 
 
     //메인 피드상에서 타임라인 페이징 처리해서 조회하는 메서드
-    List<MainTimelinePhotoDto> searchTimelineOrderBylatestPaging(Pageable pageable) throws Exception;
+    List<MainTimelinePhotoDtoRes> searchTimelineOrderBylatestPaging(Pageable pageable) throws Exception;
 
     //내타임라인 페이징 처리해서 조회
-    List<MainTimelinePhotoDto> searchMyTimelineWithPaging(Long uid, Pageable pageable) throws Exception;
+    List<MainTimelinePhotoDtoRes> searchMyTimelineWithPaging(User user, Pageable pageable) throws Exception;
 
     //다른 유저의 피드에서 타임라인 조회 with Paging
-    List<MainTimelinePhotoDto> searchTimelineNotPublicWithPaging(Long uid, Pageable pageable) throws Exception;
+    List<MainTimelinePhotoDtoRes> searchTimelineNotPublicWithPaging(Long uid, Pageable pageable) throws Exception;
 
 
     //타임 라인 하나 불러올시에, 썸네일, 시작 위치 끝나는 위치,
