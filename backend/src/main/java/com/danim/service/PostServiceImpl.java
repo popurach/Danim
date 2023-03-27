177f2c34f2c97545cb274e2bffaebc6c131000b2
woojin <woojin1home@naver.com>
2023-03-27 14:22:24 +0900

    [BE]fix: voicelength 추출하는 작업 및 에러 처리

--- backend/src/main/java/com/danim/service/PostServiceImpl.java
+++ backend/src/main/java/com/danim/service/PostServiceImpl.java
@@ -23,7 +23,15 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
 import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 
@@ -50,11 +58,36 @@ public class PostServiceImpl implements PostService {
     @Override
     public Post insertPost(Post savedPost, List<Photo> photoList, MultipartFile flagFile, MultipartFile voiceFile, InsertPostReq insertPostReq) throws Exception {
 
+        //파일 형식과 길이를 파악을 하여 post를 등록 시킬지 안시킬지 정하는 부분이다
+        String fileName = voiceFile.getOriginalFilename();
+        String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
+        if (!ext.equals("wav"))
+            throw new BaseException(ErrorMessage.NOT_PERMIT_VOICE_SAVE);
+
+        //backend 폴더 하위에 temp라는 폴더를 생성, 즉 해당 되는 폴더가 없으면 임의로 생성을 한다는 것을 의미를 함
+        Files.createDirectories(Paths.get("temp"));
+        Path target = (Path) Paths.get("temp", voiceFile.getOriginalFilename());//파일이름으로 파일을 저장을 하고자 하며 , 경로를 의미
+        try (InputStream inputStream = voiceFile.getInputStream()) {
+            Files.copy(inputStream, target, StandardCopyOption.REPLACE_EXISTING);//파일 저장
+        } catch (Exception e) {
+            throw new BaseException(ErrorMessage.NOT_PERMIT_VOICE_SAVE);
+        }
+
+        File file = new File(target.toUri());
+        AudioFileFormat audioInputStream = AudioSystem.getAudioFileFormat(file);
+        AudioFormat format = audioInputStream.getFormat();
+        long frameLength = audioInputStream.getFrameLength();
+        double durationInSeconds = (frameLength / format.getFrameRate());
+        Files.delete(target);//파일을 삭제하는 코드임
+        int check_time = (int) durationInSeconds;
+        //System.out.println("Duration: " + durationInSeconds + " seconds");
+        if (check_time > 30) {
+            throw new BaseException(ErrorMessage.OVER_VOICE_TIME);
+        }
+
+
         // voiceFile S3에 올리고 voiceURL 가져오기
         String voiceUrl = awsS3.upload(voiceFile, "Danim/Voice");
-
-        // voiceFile에서 voiceLength 가져오기
-//        Double voiceLength = VoiceUtils.getVoiceFileLength(voiceFile);
 
         // voiceFile -> text 변환
         final ClovaSpeechClient clovaSpeechClient = new ClovaSpeechClient();
@@ -79,11 +112,12 @@ public class PostServiceImpl implements PostService {
             nationRepository.save(nation);
         }
 
+
         // imageURL, voiceURL db에 저장하기
         log.info("Starting savePost transaction");
         savedPost.setPhotoList(photoList);
         savedPost.setVoiceUrl(voiceUrl);
-        savedPost.setVoiceLength(2.2);
+        savedPost.setVoiceLength(durationInSeconds);
         savedPost.setNationUrl(nation.getNationUrl());
         savedPost.setAddress1(insertPostReq.getAddress1());
         savedPost.setAddress2(insertPostReq.getAddress2());
