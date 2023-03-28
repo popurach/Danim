b51079fe22952d0837ddea904cbeb4d531736c88
kimjina <misukj68@naver.com>
2023-03-28 10:14:32 +0900

    [BE] feat: 유효성 검사 및 api 테스트 중

--- backend/src/main/java/com/danim/dto/InsertPostReq.java
+++ backend/src/main/java/com/danim/dto/InsertPostReq.java
@@ -5,6 +5,7 @@ import com.danim.exception.ErrorMessage;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
@@ -13,17 +14,18 @@ import java.util.List;
 @Setter
 @ToString
 public class InsertPostReq {
+    @NotNull
     private Long timelineId;
+    @NotNull
     private Double lat;
+    @NotNull
     private Double lng;
+    @NotNull
     private String address1;
+    @NotNull
     private String address2;
+    @NotNull
     private String address3;
+    @NotNull
     private String address4;
-
-    public void validate() {
-        if (timelineId == null || address1 == null) {
-            throw new BaseException(ErrorMessage.VALIDATION_FAIL_EXCEPTION);
-        }
     }
-}
