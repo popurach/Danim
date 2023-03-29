08bb5384b8ed7ba869bafe801ba5893dd120e135
Jul <lnacles@naver.com>
2023-03-29 11:29:29 +0900

    [BE] modify/data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,16 +1,16 @@
-insert into user(nickname,client_id,profile_image_url,password)
+insert into user(nickname,client_id,profile_image_url,password,role)
 values
-('송지율','2725446611','http://k.kakaocdn.net/dn/dIUOxh/btrOfbMpO9p/JikTtvK5PtI5Wi6RMyPkDK/img_640x640.jpg','{bcrypt}$2a$10$/Gb020QJB.9SfcUPycuLpunrWYJJ/UN77XGeeUSiMxfr6amZWh2QG'),
-('김지나','111111','http//www.','ww'),
-('이호준','222222','http//www.','ww'),
-('정우진','2725397433','http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg','{bcrypt}$2a$10$LBsFE6fKaegvaFYSSvjshOVwzuVMBd40/1B2NkKrYbwzHCp4cFzCG');
+('송지율','2725446611','http://k.kakaocdn.net/dn/dIUOxh/btrOfbMpO9p/JikTtvK5PtI5Wi6RMyPkDK/img_640x640.jpg','{bcrypt}$2a$10$/Gb020QJB.9SfcUPycuLpunrWYJJ/UN77XGeeUSiMxfr6amZWh2QG','USER'),
+('지나','2726508714','http://k.kakaocdn.net/dn/bxPduL/btrRADTVe4q/e5Vzu3IG5vqAWKzudkb77K/img_640x640.jpg','ww','USER'),
+('이호준','222222','http//www.','ww','USER'),
+('정우진','2725397433','http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg','{bcrypt}$2a$10$LBsFE6fKaegvaFYSSvjshOVwzuVMBd40/1B2NkKrYbwzHCp4cFzCG','USER');
 
 insert into time_line(user_uid,create_time ,modify_time ,complete,timeline_public)
 values
-(1,now(),now(),0,1),
-(2,now(),now(),0,1),
-(3,now(),now(),0,1),
-(4,now(),now(),0,1);
+(1,now(),now(),1,1),
+(2,now(),now(),1,1),
+(3,now(),now(),1,1),
+(4,now(),now(),1,1);
 
 insert into nation(name,nation_url)
 values
@@ -71,4 +71,6 @@ now());
 insert into photo(post_id,photo_id,is_live,photo_url)
 values
 (1,1,0,'https://mydanimbucket.s3.ap-northeast-2.amazonaws.com/Danim/Post/ceb0be9a-3bee-4cb8-9685-0288dad5d323.png'),
-(2,2,0,'https://mydanimbucket.s3.ap-northeast-2.amazonaws.com/Danim/Post/0fea5d6d-61e1-4c3c-a1fb-92b73e6a8b03.png');
+(2,2,0,'https://mydanimbucket.s3.ap-northeast-2.amazonaws.com/Danim/Post/0fea5d6d-61e1-4c3c-a1fb-92b73e6a8b03.png'),
+(3,3,0,'https://mydanimbucket.s3.ap-northeast-2.amazonaws.com/Danim/Post/ceb0be9a-3bee-4cb8-9685-0288dad5d323.png'),
+(4,4,0,'https://mydanimbucket.s3.ap-northeast-2.amazonaws.com/Danim/Post/0fea5d6d-61e1-4c3c-a1fb-92b73e6a8b03.png');
