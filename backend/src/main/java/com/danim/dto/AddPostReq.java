c05f1e7b3e6bcd7905addc1b03d0849a069d91ea
kimjina <misukj68@naver.com>
2023-03-29 13:12:20 +0900

    [BE] feat: lat,lng 삭제

--- backend/src/main/java/com/danim/dto/AddPostReq.java
+++ backend/src/main/java/com/danim/dto/AddPostReq.java
@@ -14,8 +14,6 @@ import javax.validation.constraints.NotNull;
 public class AddPostReq {
     @NotNull(message = "[Request] timelineId 값을 입력해주세요.")
     private Long timelineId;
-    private Double lat;
-    private Double lng;
     @NotNull(message = "[Request] address1 값을 입력해주세요.")
     private String address1;
     private String address2;
