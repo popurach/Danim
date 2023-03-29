7904a9ff504a5cf6f6391c5f23040c42ea72bee6
kimjina <misukj68@naver.com>
2023-03-29 17:42:40 +0900

    [BE] feat: FavoriteController에서 @RequestParam -> @RequestBody로 수정, post등록 return 값 수정

---
+++ backend/src/main/java/com/danim/dto/ChangeFavoriteStatusReq.java
@@ -0,0 +1,17 @@
+package com.danim.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ChangeFavoriteStatusReq {
+        @NotNull(message = "[Request] userUid 값을 입력해주세요.")
+        private Long userUid;
+        @NotNull(message = "[Request] postId 값을 입력해주세요.")
+        private Long postId;
+}
