af58a6c01c521d1975ca12d3092be8a05ebb5d06
kimjina <misukj68@naver.com>
2023-03-30 17:04:07 +0900

    [BE] feat : 좋아요 기능 - accessToken에서 userUid 추출로 변경

--- backend/src/main/java/com/danim/controller/FavoriteController.java
+++ backend/src/main/java/com/danim/controller/FavoriteController.java
@@ -2,13 +2,18 @@ package com.danim.controller;
 
 import com.danim.dto.ChangeFavoriteStatusRes;
 import com.danim.dto.ChangeFavoriteStatusReq;
+import com.danim.dto.UserInfoRes;
 import com.danim.entity.Post;
+import com.danim.entity.User;
 import com.danim.exception.BaseException;
 import com.danim.exception.ErrorMessage;
 import com.danim.service.FavoriteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -25,9 +30,18 @@ public class FavoriteController {
     // 해당 유저가 포스트에 좋아요를 취소한 경우 -> favorite 삭제 (false 반환)
     @PostMapping(value = "")
     public ResponseEntity<?> changeFavoriteStatus(@RequestBody @Valid ChangeFavoriteStatusReq changeFavoriteStatusReq) throws Exception {
+        // accessToken에서 userUid 값 가져오기
+        Long userUid = null;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null && auth.getPrincipal() != null) {
+            User user = (User) auth.getPrincipal();
+            userUid = user.getUserUid();
+        }
+
+        // 응답
         ChangeFavoriteStatusRes res = new ChangeFavoriteStatusRes();
         res.setPostId(changeFavoriteStatusReq.getPostId());
-        res.setFavorite(favoriteService.isFavorite(changeFavoriteStatusReq.getPostId(), changeFavoriteStatusReq.getUserUid()));
+        res.setFavorite(favoriteService.isFavorite(changeFavoriteStatusReq.getPostId(), userUid));
         res.setTotalFavorite(favoriteService.countFavorite(changeFavoriteStatusReq.getPostId()));
         return ResponseEntity.ok(res);
     }
--- backend/src/main/java/com/danim/dto/ChangeFavoriteStatusReq.java
+++ backend/src/main/java/com/danim/dto/ChangeFavoriteStatusReq.java
@@ -10,8 +10,8 @@ import javax.validation.constraints.NotNull;
 @Setter
 @ToString
 public class ChangeFavoriteStatusReq {
-        @NotNull(message = "[Request] userUid 값을 입력해주세요.")
-        private Long userUid;
+//        @NotNull(message = "[Request] userUid 값을 입력해주세요.")
+//        private Long userUid;
         @NotNull(message = "[Request] postId 값을 입력해주세요.")
         private Long postId;
 }
