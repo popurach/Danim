4788d9ef24f636f2f561cfae5546178c5afe6c98
이호준 <2hojune@naver.com>
2023-04-05 15:01:12 +0900

    [BE] fix: redisConfig 수정

--- backend/src/main/java/com/danim/config/RedisConfig.java
+++ backend/src/main/java/com/danim/config/RedisConfig.java
@@ -2,18 +2,30 @@ package com.danim.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+@Configuration
+@EnableRedisRepositories
 public class RedisConfig {
     @Value("${spring.redis.host}")
-    private String host;
+    private String redisHost;
 
     @Value("${spring.redis.port}")
-    private int port;
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPassword(redisPassword);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 }
