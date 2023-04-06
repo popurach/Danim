52608ee9d3671faae6c4775bdf9f45559e3ab186
기남석 <kee0304@naver.com>
2023-04-06 17:49:10 +0900

    [FE] feat : 위치 불러올 때 돌아감

--- frontend/lib/views/record_screen.dart
+++ frontend/lib/views/record_screen.dart
@@ -37,121 +37,134 @@ class RecordView extends StatelessWidget {
                       children: [
                         // 캐러셀
                         SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.575,
+                          height: MediaQuery.of(context).size.height * 0.55,
                           // 컨슈머로 변화 감지
                           child: ImagesPageView(
                               listXFile: recordViewModel.imageList),
                         ),
-                        Container(
-                            margin: const EdgeInsets.only(top: 25),
-                            padding: const EdgeInsets.only(left: 30, right: 30),
-                            height: 30,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: recordViewModel.locationInfo.flag !=
-                                          null
-                                      ? ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(20),
-                                          child: Image.memory(
-                                            recordViewModel.locationInfo.flag!,
-                                            fit: BoxFit.fitHeight,
-                                          ),
-                                        )
-                                      : Container(),
-                                ),
-                                const SizedBox(
-                                  width: 10,
-                                ),
-                                Expanded(
-                                    flex: 5,
-                                    child: Text(
-                                        '${recordViewModel.locationInfo.country} ${recordViewModel.locationInfo.address2}'))
-                              ],
-                            )),
-                        Container(
-                          margin: const EdgeInsets.only(top: 10),
-                          child: ChangeNotifierProvider<AudioPlayerViewModel>(
-                            create: (_) => recordViewModel.audioPlayerViewModel,
-                            child: AudioPlayerView(),
-                          ),
-                        ),
-                        // 버튼 컨테이너
-                        Container(
-                          padding: const EdgeInsets.only(top: 26),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                margin: const EdgeInsets.only(left: 50),
-                                width: 50,
-                                height: 50,
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.green,
-                                ),
-                                child: IconButton(
-                                  onPressed: () {
-                                    if (recordViewModel.imageList.length >= 9) {
-                                      OneButtonMaterialDialog().showFeedBack(
-                                          context,
-                                          "이미지는 \n최대 9장까지 \n등록 가능합니다.");
-                                    } else {
-                                      recordViewModel.uploadFileFromGallery();
-                                    }
-                                  },
-                                  icon: const Icon(Icons.photo_outlined),
-                                  color: Colors.white,
-                                ),
-                              ),
-
-                              // 녹음 버튼
-                              SizedBox(
-                                width: 70,
-                                height: 70,
-                                child: ColoredContainer(
-                                  defaultColor: Colors.redAccent,
-                                  highlightColor: Colors.greenAccent,
-                                  onTapDown: () {
-                                    recordViewModel.startRecording();
-                                  },
-                                  onTapUp: () {
-                                    recordViewModel.stopRecording();
-                                  },
-                                  child: const Icon(
-                                    Icons.multitrack_audio,
-                                    color: Colors.white,
-                                    size: 50,
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(top: 25),
+                              padding: const EdgeInsets.only(left: 30, right: 30),
+                              height: 30,
+                              child:
+                              !recordViewModel.havingLocation ?
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: recordViewModel.locationInfo.flag !=
+                                        null
+                                        ? ClipRRect(
+                                      borderRadius:
+                                      BorderRadius.circular(20),
+                                      child: Image.memory(
+                                        recordViewModel.locationInfo.flag!,
+                                        fit: BoxFit.fitHeight,
+                                      ),
+                                    )
+                                        : Container(),
                                   ),
-                                ),
-                              ),
-
-                              Container(
-                                margin: const EdgeInsets.only(right: 50),
-                                width: 50,
-                                height: 50,
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.blueAccent,
-                                ),
-                                child: IconButton(
-                                  onPressed: () {
-                                    recordViewModel.uploadConfirm(
-                                      context,
-                                      appViewModel.userInfo,
-                                      appViewModel.goToTravelingTimelinePage,
-                                    );
-                                  },
-                                  icon: const Icon(Icons.upload, size: 28),
-                                  color: Colors.white,
-                                ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Expanded(
+                                      flex: 5,
+                                      child: Text(
+                                          '${recordViewModel.locationInfo.country} ${recordViewModel.locationInfo.address2}'))
+                                ],
                               )
-                            ],
-                          ),
-                        ),
+                                  : const SizedBox(
+                                  width: 30,
+                                  child: CircularProgressIndicator(strokeWidth: 3.0,))
+                              ,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(top: 10),
+                              child: ChangeNotifierProvider<AudioPlayerViewModel>(
+                                create: (_) => recordViewModel.audioPlayerViewModel,
+                                child: AudioPlayerView(),
+                              ),
+                            ),
+                            // 버튼 컨테이너
+                            Container(
+                              padding: const EdgeInsets.only(top: 26),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.only(left: 50),
+                                    width: 50,
+                                    height: 50,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.green,
+                                    ),
+                                    child: IconButton(
+                                      onPressed: () {
+                                        if (recordViewModel.imageList.length >= 9) {
+                                          OneButtonMaterialDialog().showFeedBack(
+                                              context,
+                                              "이미지는 \n최대 9장까지 \n등록 가능합니다.");
+                                        } else {
+                                          recordViewModel.uploadFileFromGallery();
+                                        }
+                                      },
+                                      icon: const Icon(Icons.photo_outlined),
+                                      color: Colors.white,
+                                    ),
+                                  ),
+
+                                  // 녹음 버튼
+                                  SizedBox(
+                                    width: 70,
+                                    height: 70,
+                                    child: ColoredContainer(
+                                      defaultColor: Colors.redAccent,
+                                      highlightColor: Colors.greenAccent,
+                                      onTapDown: () {
+                                        recordViewModel.startRecording();
+                                      },
+                                      onTapUp: () {
+                                        recordViewModel.stopRecording();
+                                      },
+                                      child: const Icon(
+                                        Icons.multitrack_audio,
+                                        color: Colors.white,
+                                        size: 50,
+                                      ),
+                                    ),
+                                  ),
+
+                                  Container(
+                                    margin: const EdgeInsets.only(right: 50),
+                                    width: 50,
+                                    height: 50,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.blueAccent,
+                                    ),
+                                    child: IconButton(
+                                      onPressed: () {
+                                        recordViewModel.uploadConfirm(
+                                          context,
+                                          appViewModel.userInfo,
+                                          appViewModel.goToTravelingTimelinePage,
+                                        );
+                                      },
+                                      icon: const Icon(Icons.upload, size: 28),
+                                      color: Colors.white,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+
                       ],
                     ),
                     !recordViewModel.isUploading
